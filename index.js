50a3b83eef96f6edcd4b10a5a46490a6b3bb25ff
Keith Beckman <keith.beckman@mobilelabsinc.com>
2015-12-30 01:07:31 -0500

    begin lib-ifying

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = require('./lib/main.js');
+var Mbox2Epub = require('./lib/main.js');
+
+
+var useText = true;
+
+var mbpath = './data/Aro Course.mbox';
+var outpath = './data/Aro Course.epub';
+var outdir = './data/Aro Course';
+
+var opts = {
+	use_text: false,
+	strip_background_images: true
+};
+
+new Mbox2Epub(mbpath, outpath, 'Aro Meditation Course', opts);
